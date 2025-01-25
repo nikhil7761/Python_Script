646a028d799f1d5cb703ec82abe819fbc583a476
nikhil.vargude <nikhilvargude@gmail.com>
2025-01-25 14:37:48 +0530

    added python programs

---
+++ float.py
@@ -0,0 +1,4 @@
+a=4.5
+
+
+print(float(a))
---
+++ ifelseloop.py
@@ -0,0 +1,9 @@
+a=8
+b=6
+print('the value' +str(a))
+if a>b:
+    print(str(a)+ 'is grater than' +str(b))
+else:
+    print(str(b)+ 'is grater than' +str(a))
+
+    input('please Enter to Exit')
